fb1eda68bb12ce837819c121fc7552fedfb1414a
Lucas Neves <lucas.neves@codeblox.com.br>
2021-01-03 23:56:28 -0300

    Postind data to server

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,45 +1,48 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
-import './FullPost.css';
+import "./FullPost.css";
 
 class FullPost extends Component {
 	state = {
-		loadedPost: null
-	}
+		loadedPost: null,
+	};
 	componentDidUpdate() {
 		if (this.props.id) {
-			if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-
-				axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
-				.then(response => this.setState({
-					loadedPost: response.data
-				}))
+			if (
+				!this.state.loadedPost ||
+				(this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+			) {
+				axios
+					.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+					.then((response) =>
+						this.setState({
+							loadedPost: response.data,
+						})
+					);
 			}
 		}
 	}
-	render () {
-		let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-		
+	render() {
+		let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+
 		if (this.props.id) {
-			post = <p style={{textAlign: 'center'}}>Loading...</p>
+			post = <p style={{ textAlign: "center" }}>Loading...</p>;
 		}
 
 		if (this.state.loadedPost) {
-			
 			post = (
 				<div className="FullPost">
-					<h1>{ this.state.loadedPost.title }</h1>
-					<p>{ this.state.loadedPost.content }</p>
+					<h1>{this.state.loadedPost.title}</h1>
+					<p>{this.state.loadedPost.content}</p>
 					<div className="Edit">
 						<button className="Delete">Delete</button>
 					</div>
 				</div>
-				
-				);
-			}
-			return post;
+			);
 		}
+		return post;
 	}
-	
-	export default FullPost;
+}
+
+export default FullPost;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,53 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
-import axios from 'axios';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+import axios from "axios";
 
 class Blog extends Component {
 	state = {
 		posts: [],
-		selectedPostId: null
-	}
+		selectedPostId: null,
+	};
 	componentDidMount() {
-		axios.get('https://jsonplaceholder.typicode.com/posts')
-			.then((response) => {
-				const posts = response.data.slice(0, 4);
-				const updatedPosts = posts.map(post => {
-					return {
-						...post,
-						author: 'Max'
-					}
-				})
-				this.setState({
-					posts: updatedPosts
-				})
-			})
+		axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+			const posts = response.data.slice(0, 4);
+			const updatedPosts = posts.map((post) => {
+				return {
+					...post,
+					author: "Max",
+				};
+			});
+			this.setState({
+				posts: updatedPosts,
+			});
+		});
 	}
 
 	postSelected = (id) => {
-		this.setState({ selectedPostId: id})
-	}
-	render () {
-		const posts = this.state.posts.map(post => {
+		this.setState({ selectedPostId: id });
+	};
+	render() {
+		const posts = this.state.posts.map((post) => {
 			return (
-				<Post 
-					title={post.title} key={post.id}
+				<Post
+					title={post.title}
+					key={post.id}
 					author={post.author}
 					clicked={() => this.postSelected(post.id)}
 				/>
-			) 
-				
-		})
+			);
+		});
 		return (
 			<div>
-				<section className="Posts">
-					{ posts }
-				</section>
+				<section className="Posts">{posts}</section>
 				<section>
-					<FullPost id={this.state.selectedPostId}/>
+					<FullPost id={this.state.selectedPostId} />
 				</section>
 				<section>
 					<NewPost />
